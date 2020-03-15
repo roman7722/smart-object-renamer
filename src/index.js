4b41cad0432b4c1279834acea5e90b074f1b6da9
Roman Karas <roman7722@yandex.ru>
2020-03-15 13:37:53 +0300

    Added files renaming

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import * as changeCase from "change-case";
-import replace from "replace-in-file"
+import replace from "replace-in-file";
+import shell from "shelljs";
 
-const pathToProject = "/home/rkaras/Projects/my/smart-object-renamer/TEMP/"
+const fromStr = "old-name";
+const toStr = "new-name";
 
-const fromStr = "family-status";
-const toStr = "super-category";
+const pathToProject = `/path/to/folder/with/project`;
+
+const RegExpStr = `'s/(\\b${fromStr}\\b)(?!.*\\1)/${toStr}/g'`;
 
 const files = [
   `${pathToProject}${fromStr}/*`,
@@ -15,27 +18,27 @@ const files = [
 
 const  cases = {
   camel: {
-    from: new RegExp(changeCase.camelCase(fromStr), 'g'),
+    from: new RegExp(changeCase.camelCase(fromStr), "g"),
     to: changeCase.camelCase(toStr),
   },
 
   snake: {
-    from: new RegExp(changeCase.snakeCase(fromStr), 'g'),
+    from: new RegExp(changeCase.snakeCase(fromStr), "g"),
     to: changeCase.snakeCase(toStr),
   },
 
   param: {
-    from: new RegExp(changeCase.paramCase(fromStr), 'g'),
+    from: new RegExp(changeCase.paramCase(fromStr), "g"),
     to: changeCase.paramCase(toStr),
   },
 
   pascal: {
-    from: new RegExp(changeCase.pascalCase(fromStr), 'g'),
+    from: new RegExp(changeCase.pascalCase(fromStr), "g"),
     to: changeCase.pascalCase(toStr),
   },
 
   constant: {
-    from: new RegExp(changeCase.constantCase(fromStr), 'g'),
+    from: new RegExp(changeCase.constantCase(fromStr), "g"),
     to: changeCase.constantCase(toStr),
   }
 };
@@ -69,11 +72,12 @@ const options = {
 
 try {
   const results = replace.sync(options);
-  console.log('Replacement results:', results);
+  console.log("Replacement results:", results);
+  shell.exec(`find ${pathToProject} -print0 | xargs -0 -n 1 | tac | rename ${RegExpStr}`, {silent:true});
 }
 catch (error) {
-  console.error('Error occurred:', error);
+  console.error("Error occurred:", error);
 }
 
 console.log("____________________________________________________");
-console.log('Done!');
+console.log("Done!");
